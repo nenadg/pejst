89d5bcb60b84aa08b17cac52db84743292cffb8a
nenadg <nenadgolubovic@gmail.com>
2013-11-07 13:20:06 +0100

    line numbering

--- public/js/default.js
+++ public/js/default.js
@@ -1,9 +1,10 @@
 ;(function(){
-
-    var chars, lines, words, rows, line, textarea, text, word;
+    
+    var chars, lines, words, rows, cols, line, textarea, text, word, ulist, list;
     var events = [ 'keyup', 'keydown', 'keypress' ];
     
     function e(elem, attr, val){
+        
         var element = document.getElementsByTagName(elem)[0] || document.getElementById(elem);
         
         if(element)
@@ -17,31 +18,72 @@
         else
             throw new Error('moo...')
     }
+    
+    function numerate(evt){
+        
+        var caretPosition = text.substr(0, textarea.selectionStart).split("\n").length;
+            
+        // remove highlight when line becomes accessible
+        if(evt.keyCode == '13'){
+            if(list[caretPosition -1].style.backgroundColor != '')
+                list[caretPosition -1].style.backgroundColor = '#efefef'; }
+        
+        // remove line numbers 
+        else if(evt.keyCode == '8' && text.split('\n')[line] == ''){
+            var toRemove = [];
+            
+            // mark unreachable lines (occures when ctrl+a delete, etc.) for deletion
+            for(var item = 0; item <= list.length -1; item++)
+                if(item > (caretPosition -1))
+                    toRemove.push(list[item]);
+            
+            // kill 'em all
+            for(var dead in toRemove)
+                ulist.removeChild(toRemove[dead]);
+            
+            // highligth last (unaccessible) child 
+            if(list.length > line && list.length >1)    
+                ulist.lastChild.style.background = '#dfdfdf';
+        }
+    }
 
     function count(){
-        text = textarea.value;
-        line = (text.match(/\n/g) != null)? text.match(/\n/g).length + 1 : 1;
-        word = (text.match(/\S+/g) != null)? text.match(/\S+/g).length : 0;
         
-        chars.innerHTML = text.length || 0;
-        lines.innerHTML = line || 1;
-        words.innerHTML = word || 0;   
-
-        if((line + 1) >= rows || ((rows -2) >= line && rows > 19))
-            e('textarea', 'rows', line + 2);
+        text = textarea.value;
+        line = (text.match(/\n/g)||[]).length;
+        word = (text.match(/\S+/g)||[]).length;
+        
+        chars.innerHTML = text.length;
+        lines.innerHTML = line +1;
+        words.innerHTML = word;    
+        
+        // add new lines when bottom is reached
+        if(line >= rows || ((rows -1) >= line && rows > 19))
+            e('textarea', 'rows', line + 2);    
+        
+        // add numeration according to number of lines
+        while(list.length -1 <= line)
+            ulist.innerHTML += '<li></li>';
     }
     
     return setTimeout(function(){
-
+        
         textarea = e('textarea');
+        ulist = e('ul');
+        list = ulist.getElementsByTagName('li'); 
         chars = e('chars');
         lines = e('lines');
         words = e('words');  
-        rows = e('textarea', 'rows');   
+        rows = e('textarea', 'rows');
+        cols = e('textarea', 'cols');
+        text = textarea.value;
         
-        for(var event in events)
-            e('textarea').addEventListener(events[event], count, false);  
-        
+        e('textarea').addEventListener('keypress' , function(evt){
+            count(); 
+            (!evt)? numerate(window.event): numerate(evt);
+            
+        });
+
         count();
     }, 100);
 
